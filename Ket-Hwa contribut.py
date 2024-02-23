3580f97d3682dfe9dd635c67d23e6412fbff29c6
MorningDuringNight <zexion134@gmail.com>
2024-02-23 10:38:05 -0500

    I changed some stuff

---
+++ Ket-Hwa contribut.py
@@ -0,0 +1,6 @@
+#created by Ket-Hwa Cheng for purposes of Big ideas
+name1 = input("Please enter your name")
+name2 = input("2nd member of group")
+name3 = input("3rd memeber of group")
+
+print("Members of group are: ", name1, ", ", name2, ", ", name3)
